a1ff7591c97a7ce017e90982385f00783c21d8f2
Sagar Kalligudd <kalligudd.sagar@gmail.com>
2019-02-10 21:43:08 +0530

    .

--- src/array_rotation.cpp
+++ src/array_rotation.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
   int i = 9, j = 0;
-  int arr[9] = {0,1,2,3,4,5,6,7,8,9};
+  int arr[10] = {0,1,2,3,4,5,6,7,8,9};
   int temp[10];
   while(i >= 0){
     temp[j] = arr[i];
