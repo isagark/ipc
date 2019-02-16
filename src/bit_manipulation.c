90b0f7086790e4cd8accc49280d76de10e429250
Sagar Kalligudd <kalligudd.sagar@gmail.com>
2019-02-17 00:14:06 +0530

    updating

--- src/bit_manipulation.c
+++ src/bit_manipulation.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 int count_set(int num){
   int count = 0;
   while(num){
@@ -8,10 +9,36 @@ int count_set(int num){
   return count;
 }
 
+int set_bit(int num, int pos){
+    num = num | (1 << pos);
+    return num;
+}
+
+int clear_bit(int num, int pos){
+  num = num & ~(1 << pos);
+  return num;
+}
+
+int toggle_bit(int num, int pos){
+  num = num ^ (1 << pos);
+  return num;
+}
 int main(){
-  int num = 16;
+  int num = 1;
   int count = 0;
   count = count_set(num);
-  printf("%d",count);
+  int num2 = set_bit(num,3);
+  int count2 = count_set(num2);
+  printf("%d\n",count);
+  printf("%d\n",num2);
+  printf("%d\n",count2);
+  int num3 = clear_bit(num,3);
+  int count3 = count_set(num3);
+  printf("%d\n",num3);
+  printf("%d\n",count3);
+  int num4 = toggle_bit(num,3);
+  int count4 = count_set(num4);
+  printf("%d\n",num4);
+  printf("%d\n",count4);
   return 0;
 }
